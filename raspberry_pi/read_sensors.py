fe6887eb010f9f46f69c0ca30bd924d704de3614
Philipp Bliznuk <philipp.bliznuk@cshisw.com>
2018-07-23 13:57:37 +0300

    Send average sensors data per period

--- raspberry_pi/read_sensors.py
+++ raspberry_pi/read_sensors.py
@@ -5,12 +5,15 @@ import time
 import json
 import redis
 import serial
+from collections import defaultdict
 
 
 ser = serial.Serial("/dev/ttyACM0", 9600)  #change ACM number as found from ls /dev/tty/ACM*
 ser.baudrate = 9600
 redis = redis.StrictRedis(host='localhost', port=6379)
 pubsub = redis.pubsub()
+PUBLISH_TIMEOUT = 300  # 5 mins
+TIMEOUT_BETWEEN_READ = 2
 SENSORS_CHANNEL = 'sensors_data'
 
 
@@ -29,7 +32,7 @@ SENSORS = {
     },
     'soil_humidity': {
         'pattern': r'Soil Humidity:\s+(\d+)', 
-        'callback': lambda value: round((1024 - float(value))/1024*100, 2)
+        'callback': lambda value: round((1024 - float(value)) / 1024 * 100, 2)
     },
     'temp': {
         'pattern': r'Temperature:\s+(\d+)', 
@@ -44,25 +47,22 @@ SENSORS = {
 
 # Publish to the same topic in a loop forever
 while True:
-    try:
+    payload = defaultdict(list)
+    for _ in range(PUBLISH_TIMEOUT / TIMEOUT_BETWEEN_READ):
         line = ser.readline().decode()
-    except UnicodeDecodeError:
-        time.sleep(30)
-        continue
+        for sensor_label, sensor_dict in SENSORS.items():
+            try:
+                match = re.match(sensor_dict['pattern'], line)
+                if match:
+                    payload[sensor_label].append(sensor_dict['callback'](match.group(1)))
+            except Exception as e:
+                continue
 
-    payload = {}
-    for sensor_label, sensor_dict in SENSORS.items():
-        try:
-            match = re.match(sensor_dict['pattern'], line)
-            if match:
-                payload.update({
-                    'timestamp': int(round(time.time() * 1000)),
-                    sensor_label: sensor_dict['callback'](match.group(1))
-                })
-        except Exception as e:
-            continue
+        time.sleep(TIMEOUT_BETWEEN_READ)
 
-    if payload:
-        redis.publish(SENSORS_CHANNEL, json.dumps(payload))
-
-    time.sleep(2)
+    sensors_data = dict(
+        {label: round(sum(value)/len(value), 2) for label, value in payload.items()},
+        timestamp=int(round(time.time() * 1000))
+    )
+    payload = defaultdict(list)
+    redis.publish(SENSORS_CHANNEL, json.dumps(sensors_data))
