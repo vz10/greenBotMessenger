d2193e49dcec060458a95b09e0fe5d8f56af4b85
i.batozskiy <i.batozskiy@gmail.com>
2018-07-26 15:08:42 +0300

    Add listener to the iot hub actions

--- raspberry_pi/iot_hub_connection.js
+++ raspberry_pi/iot_hub_connection.js
@@ -4,6 +4,7 @@ const chalk = require('chalk');
 const redis = require('redis');
 const {connectionString} = require('./local_settings.js');
 const SENSORS_CHANNEL = 'sensors_data';
+const ACTIONS_CHANNEL = 'perform_action';
 
 // The sample connects to a device-specific MQTT endpoint on your IoT Hub.
 var Mqtt = require('azure-iot-device-mqtt').Mqtt;
@@ -12,6 +13,29 @@ var Message = require('azure-iot-device').Message;
 
 var client = DeviceClient.fromConnectionString(connectionString, Mqtt);
 const sub = redis.createClient()
+const pub = redis.createClient()
+
+function getData(request, response) {
+
+  function directMethodResponse(err) {
+    if(err) {
+      console.error(chalk.red('An error ocurred when sending a method response:\n' + err.toString()));
+    } else {
+        console.log(chalk.green('Response to method \'' + request.methodName + '\' sent successfully.' ));
+    }
+  }
+
+  console.log(chalk.green('Direct method payload received:'));
+  console.log(chalk.green(request.payload));
+
+  if (!request.payload) {
+    console.log(chalk.red('Invalid payload received'));
+    response.send(400, 'Invalid direct method parameter: ' + request.payload, directMethodResponse);
+  } else {
+    pub.publish(ACTIONS_CHANNEL, JSON.stringify({action: request.payload}));
+    response.send(200, 'Performing action set: ' + request.payload, directMethodResponse);
+  }
+}
 
 // Print results.
 function printResultFor(op) {
@@ -34,3 +58,5 @@ sub.on('message', function(channel, message) {
 
 // Subscribe to a channel and start handling messages
 sub.subscribe(SENSORS_CHANNEL)
+
+client.onDeviceMethod('PerformAction', getData);
